5874ec5445dfc241c1a3bf037c9ea0b35666f41d
anton-bregolas <106439112+anton-bregolas@users.noreply.github.com>
2026-01-23 23:40:55 +0100

    Flexy Tools iv: Reusable button colors & btn-links
    + Replace fixed button colors with reusable CSS variables
    + Add button-link styles for separate ABC Tools Links menu

--- service_worker.js
+++ service_worker.js
@@ -29,12 +29,12 @@
 //
 //
 // ABC Tools Lite:
-// Last updated on 2026-01-13
+// Last updated on 2026-01-23
 
 const cacheName = 'abctoolscache-3129';
 
 const CACHE_PREFIX = 'abctools';
-const CACHE_VERSION = 'lite-3129-7';
+const CACHE_VERSION = 'lite-3129-8';
 const CACHE_NAME_LITE = `${CACHE_PREFIX}${CACHE_VERSION}`;
 
 const contentToCache = [
